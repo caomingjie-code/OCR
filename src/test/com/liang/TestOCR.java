8db4e7cd1ab9a34dcd5bdff1e8159ee33fb6d7c1
caomingjie <caomingjiecode@outlook.com>
2022-02-17 12:57:06 +0800

    xxx

--- src/test/com/liang/TestOCR.java
+++ src/test/com/liang/TestOCR.java
@@ -34,8 +34,7 @@ public class TestOCR {
         // 这里对图片黑白处理,增强识别率.这里先通过截图,截取图片中需要识别的部分
         BufferedImage bi = ImageHelper.convertImageToGrayscale(bufferedImage);
 
-        // 图片锐化,自己使用中影响识别率的主要因素是针式打印机字迹不连贯,所以锐化反而降低识别率
-
+        // 图片亮度
         processImg(bi,12);
 
         // 图片放大5倍,增强识别率(很多图片本身无法识别,放大15倍时就可以轻易识,但是考滤到客户电脑配置低,针式打印机打印不连贯的问题,这里就放大5倍)
