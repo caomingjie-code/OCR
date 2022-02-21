ef0804ac780255364bb72cad0ff518683d2a807b
caomingjie <caomingjiecode@outlook.com>
2022-02-21 11:05:00 +0800

    整体重构

--- src/main/java/com/javaoffers/tess4j/pdf/ImagePDFStreamEngine.java
+++ src/main/java/com/javaoffers/tess4j/pdf/ImagePDFStreamEngine.java
@@ -101,58 +101,31 @@ public class ImagePDFStreamEngine extends PDFStreamEngine {
 
     public static void processImg(BufferedImage img, int changeValue) {
         for (int x = 0; x < img.getWidth(); x++) {
-
             for (int y = 0; y < img.getHeight(); y++) {
-
                 // 获取到rgb的组合值
-
                 int rgb = img.getRGB(x, y);
-
                 Color color = new Color(rgb);
-
                 int r = color.getRed() + changeValue;
-
                 int g = color.getGreen() + changeValue;
-
                 int b = color.getBlue() + changeValue;
-
                 if (r > 255) {
-
                     r = 255;
-
                 } else if (r < 0) {
-
                     r = 0;
-
                 }
-
                 if (g > 255) {
-
                     g = 255;
-
                 } else if (g < 0) {
-
                     g = 0;
-
                 }
-
                 if (b > 255) {
-
                     b = 255;
-
                 } else if (b < 0) {
-
                     b = 0;
-
                 }
-
                 color = new Color(r, g, b);
-
                 img.setRGB(x, y, color.getRGB());
-
-
             }
-
         }
     }
 
